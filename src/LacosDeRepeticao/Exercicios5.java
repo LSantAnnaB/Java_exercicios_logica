77d7c9507ba965816d812d26be7a174ba3094edc
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2022-12-21 08:40:03 -0300

    update exercicios da aula 3 de java Laços de repetição renomeando classe

--- src/LacosDeRepeticao/Exercicios5.java
+++ src/LacosDeRepeticao/Exercicios5.java
@@ -2,7 +2,7 @@ package LacosDeRepeticao;
 
 import java.util.Scanner;
 
-public class Exercios5 {
+public class Exercicios5 {
     /*
     * Crie um programa que leia um número do teclado até que encontre um
 número igual a zero. No final, mostre a soma dos números
